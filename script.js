66b4d171474b92b29755600e02c550f12d2cf828
jakir9324 <jakirhusen9324@gmail.com>
2025-02-11 11:51:43 +0600

    grade,system

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+let marks;
+
+        marks = -31;
+
+        if(marks>100){
+            console.log("invalid marks");
+        }
+        else if(marks<0){
+            console.log("invalid marks");
+        }
+        else if(marks>=80){
+            console.log("the grade is A+");
+        }
+        else if(marks>=70){
+            console.log("the grade ia A");
+        }
+        else if(marks>=60){
+            console.log("the grade is A-");
+        }
+        else if(marks>=50){
+            console.log("the grade is B");
+        }
+        else if(marks>=40){
+            console.log("the grade is C");
+        }
+        else if(marks>=33){
+            console.log("the grade is D");
+        }
+        else{
+            console.log("the grade is F");
+        }
+            
